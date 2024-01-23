769b1c6b50969959b50408a8ef92ad056c33473e
harshal <harshalc562@gmail.com>
2024-01-24 00:19:50 +0530

    navbar converted to dark theme

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,25 +1,38 @@
 import React, {useState} from "react";
 
 const TextForm = (props) => {
-    const[text, setText] = useState('Set text here')
+    const[text, setText] = useState('')
 
     const handleUpClick = () => {
-        console.log("uppercase is clicked" + text)
+        // console.log("uppercase is clicked" + text)
         let newText = text.toUpperCase()
         setText(newText)
     }
-
+    const handleLoClick = () => {
+        let newText = text.toLowerCase()
+        setText(newText)
+    }
     const handleOnChange = (e) => {
-        console.log("onchnage")
+        // console.log("onchnage")
         setText(e.target.value)
     }
     return (
         <>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-                <textarea className="form-control" onChange={handleOnChange} value={text} id="myBox" rows="8"></textarea>
+            <div className="container">
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" onChange={handleOnChange} value={text} id="myBox" rows="8"></textarea>
+                </div>
+                <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+            <div className="container my-3">
+                <h1>Your text summary</h1>
+                <p>{text.split(" ").length} words and {text.length}</p>
+                <p>{0.008 * text.split(" ").length} Minutes to read</p>
+                <h2>Preview : </h2>
+                <p>{text}</p>
+            </div>
         </>
     );
 };
