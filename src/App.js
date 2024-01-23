543941008a1a62d8a3ab0f28e355316ed3162552
harshal <harshalc562@gmail.com>
2024-01-24 01:01:26 +0530

    theme change functionality done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils"/>
       <div className="container my-3" >
-        <TextForm  heading="Enter Your Text To Analyze"/>
+        {/* <TextForm  heading="Enter Your Text To Analyze"/> */}
+        <About />
       </div>
       
     </>
